160d26fa8f51142124e250c029581d4710e69b83
BohdanLysniak <marttelo@ukr.net>
2024-05-11 21:27:58 +0300

    add structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import ContactForm from "../ContactForm/ContactForm.jsx";
+import ContactList from "../ContactList/ContactList.jsx";
+import SearchBox from "../SearchBox/SearchBox.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
