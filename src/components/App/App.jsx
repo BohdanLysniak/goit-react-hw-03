13cf9a8ba190f566873b9669075b631fa13b4ba5
BohdanLysniak <marttelo@ukr.net>
2024-05-13 22:43:38 +0300

    hw- done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import ContactList from "../ContactList/ContactList.jsx";
 import SearchBox from "../SearchBox/SearchBox.jsx";
+import initialContacts from "../../contacts.json";
 
 function App() {
-  const [users, setUser] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
-  ]);
+  const [users, setUser] = useState(() => {
+    const storageContacts = localStorage.getItem("contact");
+    if (storageContacts !== null) {
+      return JSON.parse(storageContacts);
+    }
+    return initialContacts;
+  });
 
-  const addContact = newContact => {
-    setUser(prevContacts => {
-      return [...prevContacts, newContact];
-    });
-  };
+  useEffect(() => {
+    localStorage.setItem("contact", JSON.stringify(users));
+  }, [users]);
 
   const [filter, setFilter] = useState("");
 
   const filterContacts = users.filter(user =>
     user.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  const addContact = newContact => {
+    setUser(prevContacts => {
+      return [...prevContacts, newContact];
+    });
+  };
 
   const deleteContact = contactId => {
     setUser(prevContacts => {
